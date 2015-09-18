79ad6bef63d07efc6bc438554948eba67fa45411
hugo <hugohuang1111@gmail.com>
2015-09-18 17:03:11 +0800

    [M] update plugins

---
+++ SoomlagrowSampleCpp/cocos2d/cocos/platform/android/java/libs/facebook_lib/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/htl/Documents/Work/sdkbox-soomlagrow-sample/SoomlagrowSampleCpp/cocos2d/cocos/platform/android/java/libs/facebook_lib/bin/AndroidManifest.xml : \
+/Users/htl/Documents/Work/sdkbox-soomlagrow-sample/SoomlagrowSampleCpp/cocos2d/cocos/platform/android/java/libs/facebook_lib/AndroidManifest.xml \
